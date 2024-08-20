aae69c57ccad9c56b1d1b5b3aafad51e4e19db9e
joki <josedavidamayar@gmail.com>
2024-08-20 03:22:57 -0500

    add a custome color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-dark": "#131515",
+        "custom-dark-translucent": "rgba(19, 21, 21, 0.5)",
+      },
+    },
   },
   plugins: [],
 };
