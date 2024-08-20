4430de0a7de17a4106f0e5d12798d196fe6d00d7
joki <josedavidamayar@gmail.com>
2024-08-20 03:25:05 -0500

    add logic to render a card depends on the link selected

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
+import HeaderLogin from "./HeaderLogin";
+import LoginCard from "./LoginCard";
+import SingupCard from "./SingupCart";
+import AboutCard from "./AboutCard";
+
 export default function Login() {
+  const [card, setCard] = useState<string>("login");
+
+  const toggleCard = (cardName: string) => {
+    setCard(cardName);
+  };
+
   return (
-    <div>
-      <h2>Prueba de login</h2>
+    <div className="min-h-screen bg-custom-dark flex flex-col">
+      <HeaderLogin toggleCard={toggleCard} card={card} />
+      {card === "login" && <LoginCard />}
+      {card === "singup" && <SingupCard />}
+      {card === "aboutus" && <AboutCard />}
     </div>
   );
-};
-
+}
