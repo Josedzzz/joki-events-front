9ec172fded625e91bc26b74ecd031c8d6c7ffa8c
joki <josedavidamayar@gmail.com>
2024-08-20 03:26:01 -0500

    estructure the header of the login

--- src/components/HeaderLogin.tsx
+++ src/components/HeaderLogin.tsx
@@ -1,7 +1,45 @@
-export default function HeaderLogin() {
+// Define a type for the props
+type HeaderLoginProps = {
+  toggleCard: (cardName: string) => void;
+  card: string;
+};
+
+export default function HeaderLogin({ toggleCard, card }: HeaderLoginProps) {
+  const linkClasses = (link: string) =>
+    `hover:text-blue-400 font-bold text-sm sm:text-md md:text-lg transition duration-300 ease-in-out ${
+      card === link
+        ? "border-b-2 border-blue-400 text-blue-400"
+        : "text-white hover:text-blue-400 font-bold text-sm sm:text-md md:text-lg"
+    }`;
+
   return (
-    <header>
-      <h1></h1>
+    <header className="mt-3 h-12 w-full flex justify-between items-center max-w-3xl mx-auto">
+      <div className="flex items-center space-x-1 gap-3">
+        <h1 className="text-xl sm:text-3xl md:text-4xl text-slate-50 font-bold">
+          Joki Events
+        </h1>
+        <i className="fa-solid fa-paper-plane text-xl sm:text-3xl md:text-4xl text-blue-400"></i>
+      </div>
+      <nav className="space-x-4 sm:space-x-8">
+        <a
+          onClick={() => toggleCard("aboutus")}
+          className={linkClasses("aboutus")}
+        >
+          About us
+        </a>
+        <a
+          onClick={() => toggleCard("singup")}
+          className={linkClasses("singup")}
+        >
+          Singup
+        </a>
+        <a
+          onClick={() => toggleCard("login")}
+          className={linkClasses("login")}
+        >
+          Login
+        </a>
+      </nav>
     </header>
   );
 }
