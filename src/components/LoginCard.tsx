04d5ac44b4bc31deb5d70340b8652b076c8242be
joki <josedavidamayar@gmail.com>
2024-08-21 01:01:17 -0500

    Add the elements for the user login

--- src/components/LoginCard.tsx
+++ src/components/LoginCard.tsx
@@ -1,3 +1,52 @@
 export default function LoginCard() {
-    return <h1>Prueba de la tarjeta de login</h1>
+  return (
+    <main className="flex items-center justify-center min-h-[calc(100vh-4rem)] p-2">
+      <div className="w-full max-w-md bg-custom-dark p-8 shadow-lg rounded-lg">
+        <h2 className="text-2xl font-bold text-blue-400 text-center mb-6">
+          Login
+        </h2>
+        <form>
+          <div className="mb-4">
+            <label
+              className="block text-slate-50 font-bold mb-2"
+              htmlFor="username"
+            >
+              <i className="fa-regular fa-envelope"></i> Email
+            </label>
+            <input
+              type="text"
+              id="username"
+              className="bg-custom-gray text-slate-50 w-full px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-slate-50 font-bold mb-2"
+              htmlFor="password"
+            >
+              <i className="fa-solid fa-lock"></i> Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="bg-custom-gray text-slate-50 w-full px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+              placeholder="Enter your password"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              type="submit"
+              className="w-full text-slate-50 font-bold p-2 border-4 border-blue-400 rounded-xl hover:bg-blue-400 transition duration-300 ease-in-out transform hover:scale-105"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+        <p className="text-gray-600 text-sm text-center mt-6">
+          Don't have an account? Just sing up
+        </p>
+      </div>
+    </main>
+  );
 }
