95d970043af0fd521c414a2fa6a0475bd32e5fb5
joki <josedavidamayar@gmail.com>
2024-08-24 02:49:38 -0500

    logic to get a user for the login

---
+++ src/services/loginService.ts
@@ -0,0 +1,43 @@
+interface LoginCredentials {
+  email: string;
+  password: string;
+}
+
+interface SuccessResponse {
+  id: string;
+}
+
+interface ErrorResponse {
+  message: string;
+}
+
+/**
+ * Promise function that gets the response for the login
+ * @param credentials interface that contains the login credentials
+ * @returns the id of the client in a json
+ */
+export const login = async (credentials: LoginCredentials): Promise<string> => {
+  const response = await fetch("http://localhost:3000/api/clients/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  });
+
+  if (!response.ok) {
+    // Handle the error response
+    const errorResponse: ErrorResponse = await response.json();
+    throw new Error(errorResponse.message);
+  }
+
+  // Read the response as a json
+  const loginResponse: SuccessResponse = await response.json();
+
+  // chek that the json contains an id
+  if ("id" in loginResponse) {
+    return loginResponse.id;
+  } else {
+    throw new Error("Unexpected response format");
+  }
+};
