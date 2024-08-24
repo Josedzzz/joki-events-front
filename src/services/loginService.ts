5f7032200a65bcc864d5897cc32abe0344d0e683
joki <josedavidamayar@gmail.com>
2024-08-24 02:59:18 -0500

    add a try catch to handle an error during the login

--- src/services/loginService.ts
+++ src/services/loginService.ts
@@ -17,27 +17,32 @@ interface ErrorResponse {
  * @returns the id of the client in a json
  */
 export const login = async (credentials: LoginCredentials): Promise<string> => {
-  const response = await fetch("http://localhost:3000/api/clients/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(credentials),
-  });
+  try {
+    const response = await fetch("http://localhost:3000/api/clients/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(credentials),
+    });
 
-  if (!response.ok) {
-    // Handle the error response
-    const errorResponse: ErrorResponse = await response.json();
-    throw new Error(errorResponse.message);
-  }
+    if (!response.ok) {
+      // Handle the error response
+      const errorResponse: ErrorResponse = await response.json();
+      throw new Error(errorResponse.message);
+    }
 
-  // Read the response as a json
-  const loginResponse: SuccessResponse = await response.json();
+    // Read the response as a json
+    const loginResponse: SuccessResponse = await response.json();
 
-  // chek that the json contains an id
-  if ("id" in loginResponse) {
-    return loginResponse.id;
-  } else {
-    throw new Error("Unexpected response format");
+    // chek that the json contains an id
+    if ("id" in loginResponse) {
+      return loginResponse.id;
+    } else {
+      throw new Error("Unexpected response format");
+    }
+  } catch (error) {
+    console.error("Error during login:", error);
+    throw error; 
   }
 };
