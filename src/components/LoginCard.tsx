d8332c4bf23856b7743f80250a03076dbd1b5981
joki <josedavidamayar@gmail.com>
2024-08-24 02:50:10 -0500

    define useStates to make the login

--- src/components/LoginCard.tsx
+++ src/components/LoginCard.tsx
@@ -1,11 +1,40 @@
+import { useState } from "react";
+import { login } from "../services/loginService";
+
 export default function LoginCard() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  /**
+   * Handles the submission of the login form
+   * @param e the form submission event
+   */
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError("");
+    setSuccess("");
+
+    try {
+      const userId = await login({ email, password });
+      setSuccess(`Welcome, user with ID: ${userId}!`);
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+      } else {
+        setError("An unexpected error occurred.");
+      }
+    }
+  };
+
   return (
     <main className="flex items-center justify-center min-h-[calc(100vh-4rem)] p-2 show-animation">
       <div className="w-full max-w-md bg-custom-dark p-8 shadow-lg rounded-lg">
         <h2 className="text-2xl font-bold text-blue-400 text-center mb-6">
           Login
         </h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label
               className="block text-slate-50 font-bold mb-2"
@@ -18,6 +47,8 @@ export default function LoginCard() {
               id="username"
               className="bg-custom-gray text-slate-50 w-full px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
               placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="mb-6">
@@ -32,6 +63,8 @@ export default function LoginCard() {
               id="password"
               className="bg-custom-gray text-slate-50 w-full px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
               placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <div className="flex items-center justify-between">
@@ -43,8 +76,14 @@ export default function LoginCard() {
             </button>
           </div>
         </form>
+
+        {error && <p className="text-red-500 text-center mt-4">{error}</p>}
+        {success && (
+          <p className="text-green-500 text-center mt-4">{success}</p>
+        )}
+
         <p className="text-gray-600 text-sm text-center mt-6">
-          Don't have an account? Just sing up
+          Don't have an account? Just sign up
         </p>
       </div>
     </main>
