d374d3ca157864ba1bf396a48c18f45d8dd96d14
joki <josedavidamayar@gmail.com>
2024-09-12 13:33:30 -0500

    first version of the interface was added so that an admin can see information about his account

--- src/components/AdminAccount.tsx
+++ src/components/AdminAccount.tsx
@@ -1,7 +1,62 @@
+// Interface for the admin
+interface Admin {
+  username: string;
+  email: string;
+}
+
 export default function AdminAccount() {
+  const adminData: Admin[] = [
+    {
+      username: "Danielito",
+      email: "danielito@email",
+    },
+  ];
+
   return (
-    <div>
-      <h1>Prueba de la cuenta del admin</h1>
+    <div className="bg-custom-black w-full min-h-[calc(100vh-4rem)] p-6 flex items-center">
+      <div className="bg-custom-dark rounded-lg shadow-lg p-6 max-w-xl mx-auto flex flex-col">
+        <h2 className="text-xl font-bold text-blue-400 mb-4">
+          Account information
+        </h2>
+        <form>
+          <div className="mb-4">
+            <label
+              className="block text-slate-50 font-bold mb-2"
+              htmlFor="email"
+            >
+              <i className="fa-regular fa-envelope mr-1"></i> Email
+            </label>
+            <input
+              type="email"
+              id="username"
+              className="bg-custom-gray text-slate-50 w-full px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+              placeholder="Enter your new email"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-slate-50 font-bold mb-2"
+              htmlFor="username"
+            >
+              <i className="fa-regular fa-user mr-1"></i> Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              className="bg-custom-gray text-slate-50 w-full px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+              placeholder="Enter your new username"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              type="submit"
+              className="w-full text-slate-50 font-bold p-2 border-4 border-blue-400 rounded-xl hover:bg-blue-400 transition duration-300 ease-in-out transform hover:scale-105"
+            >
+              Update data
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
