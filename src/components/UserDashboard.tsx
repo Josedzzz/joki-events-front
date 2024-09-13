9a53a19a293919b19e6442815d3af7c05d8e4264
joki <josedavidamayar@gmail.com>
2024-09-13 10:41:19 -0500

    add logic to render content to the user  depending on the useState content

--- src/components/UserDashboard.tsx
+++ src/components/UserDashboard.tsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
+import UserHeader from "./UserHeader";
+import UserEvents from "./UserEvents";
+import UserCart from "./UserCart";
+import UserAccount from "./UserAccount";
+
 export default function UserDashboard() {
+  // useState to set the content
+  const [content, setContent] = useState<string>("events");
+
+  /**
+   * change the window content on the menu
+   * @param contentName the name of the content to be displayed
+   */
+  const toggleContent = (contentName: string) => {
+    setContent(contentName);
+  };
+
   return (
-    <div className="p-4">
-      <h1 className="text-white text-2xl">User Dashboard</h1>
+    <div className="min-h-screen bg-custom-black flex flex-col">
+      <UserHeader toggleContent={toggleContent} content={content} />
+      {content === "events" && <UserEvents />}
+      {content === "cart" && <UserCart />}
+      {content === "account" && <UserAccount />}
     </div>
   );
 }
