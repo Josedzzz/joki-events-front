fbd9157ed72b46abef83ad74517795047894d97a
joki <josedavidamayar@gmail.com>
2024-09-27 00:28:19 -0500

    promise function to get the username and email for an admin

--- src/services/updateAdminService.ts
+++ src/services/updateAdminService.ts
@@ -1,3 +1,14 @@
+interface AdminInfo {
+  username: string;
+  email: string;
+}
+
+interface AdminInfoResponse {
+  status: string;
+  message: string;
+  data: AdminInfo;
+}
+
 interface updateAdminCredentials {
   username: string;
   email: string;
@@ -64,6 +75,53 @@ export const updateAdmin = async (
     }
   } catch (error) {
     console.error("Error during the update:", error);
+    throw error;
+  }
+};
+
+/**
+ * Promise function that gets the admin account information
+ * @returns the api response interface with the admin information
+ */
+export const getAdminAccountInfo = async (): Promise<AdminInfoResponse> => {
+  // Get the admin ID from local storage
+  const adminId = localStorage.getItem("adminId");
+
+  // Get the cookie token
+  const authToken = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith("authAdminToken="))
+    ?.split("=")[1];
+
+  try {
+    const response = await fetch(
+      `http://localhost:8080/api/admin/get-admin-account-info/${adminId}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${authToken}`,
+        },
+      },
+    );
+
+    if (!response.ok) {
+      // Handle the error response
+      const errorResponse: ApiResponse = await response.json();
+      throw new Error(errorResponse.message);
+    }
+
+    // Read the response as JSON
+    const successResponse: AdminInfoResponse = await response.json();
+
+    // Check that the json contains the expected data structure
+    if ("data" in successResponse) {
+      return successResponse;
+    } else {
+      throw new Error("Unexpected response format");
+    }
+  } catch (error) {
+    console.error("Error during fetching admin info:", error);
     throw error;
   }
 };
