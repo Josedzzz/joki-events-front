f7053abc12ff032a165c0ebfbc4b79250f069cc7
joki <josedavidamayar@gmail.com>
2024-09-28 22:28:44 -0500

    delete the current events and crate the interfaces for the events and coupons

--- src/components/AdminEvents.tsx
+++ src/components/AdminEvents.tsx
@@ -1,112 +1,27 @@
 import { useState } from "react";
-import AdminEventCard from "./AdminEventCard";
 import AdminEventInfo from "./AdminEventInfo";
 
-// Interface for the events
 export interface Event {
   id: string;
-  idDistributionLocality: string[];
   name: string;
   address: string;
   city: string;
   eventDate: string;
-  totalAvailableQuantity: number;
-  imageUrl: string;
-  localityImageUrl: string;
+  availableForPurchase: boolean;
+  localities: Locality[];
+  totalAvailablePlaces: number;
+  eventImageUrl: string;
+  eventType: string;
+}
+
+export interface Locality {
+  name: string;
+  price: number;
+  maxCapacity: number;
+  currentOccupancy: number;
 }
 
 export default function AdminEvents() {
-  const eventsData: Event[] = [
-    {
-      id: "1",
-      idDistributionLocality: ["loc1", "loc2"],
-      name: "Music Festival",
-      address: "123 Main St",
-      city: "Los Angeles",
-      eventDate: "2024-10-05T18:00",
-      totalAvailableQuantity: 10,
-      imageUrl: "/event.jpg",
-      localityImageUrl: "/event.jpg",
-    },
-    {
-      id: "2",
-      idDistributionLocality: ["loc3"],
-      name: "Art Expo",
-      address: "456 Elm St",
-      city: "New York",
-      eventDate: "2024-09-20T10:00",
-      totalAvailableQuantity: 200,
-      imageUrl: "/event.jpg",
-      localityImageUrl: "/event.jpg",
-    },
-    {
-      id: "3",
-      idDistributionLocality: ["loc4"],
-      name: "Tech Conference",
-      address: "789 Oak St",
-      city: "San Francisco",
-      eventDate: "2024-11-15T09:00",
-      totalAvailableQuantity: 34,
-      imageUrl: "/event.jpg",
-      localityImageUrl: "/event.jpg",
-    },
-    {
-      id: "4",
-      idDistributionLocality: ["loc5", "loc6"],
-      name: "Food Fair",
-      address: "101 Maple St",
-      city: "Chicago",
-      eventDate: "2024-12-01T12:00",
-      totalAvailableQuantity: 45,
-      imageUrl: "/event.jpg",
-      localityImageUrl: "/event.jpg",
-    },
-    {
-      id: "5",
-      idDistributionLocality: ["loc1", "loc2"],
-      name: "Music Festival",
-      address: "123 Main St",
-      city: "Los Angeles",
-      eventDate: "2024-10-05T18:00",
-      totalAvailableQuantity: 10,
-      imageUrl: "/event.jpg",
-      localityImageUrl: "/event.jpg",
-    },
-    {
-      id: "6",
-      idDistributionLocality: ["loc3"],
-      name: "Art Expo",
-      address: "456 Elm St",
-      city: "New York",
-      eventDate: "2024-09-20T10:00",
-      totalAvailableQuantity: 200,
-      imageUrl: "/event.jpg",
-      localityImageUrl: "/event.jpg",
-    },
-    {
-      id: "7",
-      idDistributionLocality: ["loc4"],
-      name: "Tech Conference",
-      address: "789 Oak St",
-      city: "San Francisco",
-      eventDate: "2024-11-15T09:00",
-      totalAvailableQuantity: 34,
-      imageUrl: "/event.jpg",
-      localityImageUrl: "/event.jpg",
-    },
-    {
-      id: "8",
-      idDistributionLocality: ["loc5", "loc6"],
-      name: "Food Fair",
-      address: "101 Maple St",
-      city: "Chicago",
-      eventDate: "2024-12-01T12:00",
-      totalAvailableQuantity: 45,
-      imageUrl: "/event.jpg",
-      localityImageUrl: "/event.jpg",
-    },
-  ];
-
   const [selectedEvent, setSelectedEvent] = useState<Event | null>(null);
   const [isAddingNewEvent, setIsAddingNewEvent] = useState<boolean>(false);
 
@@ -148,15 +63,7 @@ export default function AdminEvents() {
               <i className="fa-solid fa-arrow-right"></i>
             </button>
           </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-            {eventsData.map((event) => (
-              <AdminEventCard
-                key={event.id}
-                event={event}
-                onClick={() => handleSelectEvent(event)}
-              />
-            ))}
-          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4"></div>
         </div>
       ) : (
         <AdminEventInfo
