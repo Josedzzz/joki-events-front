d73650722ba5163fd856e8839ead45d01b7680b8
joki <josedavidamayar@gmail.com>
2024-10-03 02:32:48 -0500

    add animations

--- src/components/AdminAccount.tsx
+++ src/components/AdminAccount.tsx
@@ -83,7 +83,7 @@ export default function AdminAccount() {
   }, []);
 
   return (
-    <div className="bg-custom-black w-full min-h-[calc(100vh-4rem)] p-6 flex items-center">
+    <div className="bg-custom-black w-full min-h-[calc(100vh-4rem)] p-6 flex items-center show-animation">
       <div className="bg-custom-dark rounded-lg shadow-lg p-6 max-w-xl mx-auto flex flex-col">
         <h2 className="text-xl font-bold text-blue-400 mb-4">
           Account information
--- src/components/AdminCoupons.tsx
+++ src/components/AdminCoupons.tsx
@@ -79,7 +79,7 @@ export default function AdminCoupons() {
   }, []);
 
   return (
-    <div className="bg-custom-black w-full min-h-[calc(100vh-4rem)] p-6">
+    <div className="bg-custom-black w-full min-h-[calc(100vh-4rem)] p-6 show-animation">
       {!selectedCoupon && !isAddingNewCoupon ? (
         <div className="px-4 py-6">
           <div className="flex flex-row items-center gap-4 mb-2">
