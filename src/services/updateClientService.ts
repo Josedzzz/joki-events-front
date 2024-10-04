e2a17a10439971cfcb536bcc094bf638fac99948
joki <josedavidamayar@gmail.com>
2024-10-04 01:40:45 -0500

    create the service to manage the userAccount information

---
+++ src/services/updateClientService.ts
@@ -0,0 +1,131 @@
+interface ClientInfo {
+  idCard: string;
+  phone: string;
+  email: string;
+  name: string;
+  address: string;
+}
+
+interface ClientInfoResponse {
+  status: string;
+  message: string;
+  data: ClientInfo;
+}
+
+interface UpdateClientCredentials {
+  idCard: string;
+  phone: string;
+  email: string;
+  name: string;
+  address: string;
+}
+
+interface ApiTokenResponse {
+  status: string;
+  message: string;
+  data: string;
+  token: string;
+}
+
+interface ApiResponse {
+  status: string;
+  message: string;
+  data: string;
+}
+
+/**
+ * Promise function that gets the response for the updated client data
+ * @param credentials the credentials to be updated
+ * @returns the api response interface with the information about it
+ */
+export const updateClient = async (
+  credentials: UpdateClientCredentials,
+): Promise<ApiTokenResponse> => {
+  const userId = localStorage.getItem("userId");
+
+  // gets the cookie token
+  const authToken = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith("authToken="))
+    ?.split("=")[1];
+
+  try {
+    const response = await fetch(
+      `http://localhost:8080/api/clients/${userId}/update/`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${authToken}`,
+        },
+        body: JSON.stringify(credentials),
+      },
+    );
+
+    if (!response.ok) {
+      // handle the error response
+      const errorResponse: ApiResponse = await response.json();
+      throw new Error(errorResponse.message);
+    }
+
+    // read the response as a json
+    const successResponse: ApiTokenResponse = await response.json();
+
+    // checks that the json contains the message for the update
+    if ("token" in successResponse) {
+      return successResponse;
+    } else {
+      throw new Error("Unexpected response format");
+    }
+  } catch (error) {
+    console.error("Error during the update:", error);
+    throw error;
+  }
+};
+
+/**
+ * Promise function that gets the client account information
+ * @returns the ClientInfoResponse interface with all the information about it
+ */
+export const getClientAccountInfo = async (): Promise<ClientInfoResponse> => {
+  // get the user id from the local storage
+  const userId = localStorage.getItem("userId");
+
+  // gets the cookie token
+  const authToken = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith("authToken="))
+    ?.split("=")[1];
+
+  try {
+    const response = await fetch(
+      `http://localhost:8080/api/clients/get-client-account-info/${userId}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${authToken}`,
+        },
+      },
+    );
+
+    if (!response.ok) {
+      // handle the error response
+      const errorResponse: ApiResponse = await response.json();
+      throw new Error(errorResponse.message);
+    }
+
+    // read the response as a json
+    const successResponse: ClientInfoResponse = await response.json();
+
+    // Check that the json contains the expected data structure
+    if ("data" in successResponse) {
+      return successResponse;
+    } else {
+      throw new Error("Unexpected response format");
+    }
+  } catch (error) {
+    console.error("Error during fetching the user info:", error);
+    throw error;
+  }
+};
