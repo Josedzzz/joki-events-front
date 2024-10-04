10b3d07c9370ef3065262ae3443955b1ca5c88e0
joki <josedavidamayar@gmail.com>
2024-10-04 01:41:45 -0500

    render the account information

--- src/components/UserAccount.tsx
+++ src/components/UserAccount.tsx
@@ -1,4 +1,37 @@
+import { useEffect, useState } from "react";
+import { getClientAccountInfo } from "../services/updateClientService";
+
 export default function UserAccount() {
+  const [idCard, setIdCard] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [address, setAddress] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  /**
+   * fetch the client account information
+   */
+  const fetchClientInfo = async () => {
+    try {
+      const response = await getClientAccountInfo();
+      setIdCard(response.data.idCard);
+      setPhone(response.data.phone);
+      setEmail(response.data.email);
+      setUsername(response.data.name);
+      setAddress(response.data.address);
+    } catch (error) {
+      console.error("Error fetching admin info:", error);
+      setError("Failed to load the information");
+    }
+  };
+
+  // fetch the client info when the component mounts
+  useEffect(() => {
+    fetchClientInfo();
+  }, []);
+
   return (
     <div className="bg-custom-black w-full min-h-[calc(100vh-4rem)] p-6 flex items-center">
       <div className="bg-custom-dark rounded-lg shadow-lg p-6 max-w-xl mx-auto flex flex-col">
@@ -18,6 +51,8 @@ export default function UserAccount() {
               id="idCard"
               className="bg-custom-gray text-slate-50 w-full px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
               placeholder="Enter your id card"
+              value={idCard}
+              onChange={(e) => setIdCard(e.target.value)}
             />
           </div>
 
@@ -33,6 +68,8 @@ export default function UserAccount() {
               id="phone"
               className="bg-custom-gray text-slate-50 w-full px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
               placeholder="Enter your phone"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
             />
           </div>
 
@@ -48,6 +85,8 @@ export default function UserAccount() {
               id="email"
               className="bg-custom-gray text-slate-50 w-full px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
               placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
 
@@ -63,21 +102,25 @@ export default function UserAccount() {
               id="username"
               className="bg-custom-gray text-slate-50 w-full px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
               placeholder="Enter your username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
             />
           </div>
 
           <div className="mb-4">
             <label
               className="block text-slate-50 font-bold mb-2"
-              htmlFor="direction"
+              htmlFor="address"
             >
-              <i className="fa-solid fa-location-dot mr-2"></i> Direction
+              <i className="fa-solid fa-location-dot mr-2"></i> Address
             </label>
             <input
               type="text"
-              id="direction"
+              id="address"
               className="bg-custom-gray text-slate-50 w-full px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-              placeholder="Enter your direction"
+              placeholder="Enter your address"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
             />
           </div>
 
@@ -96,6 +139,10 @@ export default function UserAccount() {
             </button>
           </div>
         </form>
+        {error && <p className="text-red-500 text-center mt-4">{error}</p>}
+        {success && (
+          <p className="text-green-500 text-center mt-4">{success}</p>
+        )}
       </div>
     </div>
   );
