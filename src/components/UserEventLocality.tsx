b7cc8a05824aca5f0a55273f512c196a36867a53
joki <josedavidamayar@gmail.com>
2024-10-10 00:08:10 -0500

    render the input field of the locality if its selected

--- src/components/UserEventLocality.tsx
+++ src/components/UserEventLocality.tsx
@@ -3,15 +3,27 @@ import { Locality } from "./UserEvents";
 // Interface for the locality props
 interface UserEventLocalityProps {
   locality: Locality;
+  isSelected: boolean;
+  onSelect: () => void;
+  ticketsSelected: number;
+  setTicketsSelected: (quantity: number) => void;
+  isDisabled: boolean;
 }
 
 export default function UserEventLocality({
   locality,
+  isSelected,
+  onSelect,
+  ticketsSelected,
+  setTicketsSelected,
+  isDisabled,
 }: UserEventLocalityProps) {
   return (
     <div
-      key={locality.name}
-      className="bg-custom-gray p-2 rounded-lg shadow-md transition-transform hover:border-2 hover:border-blue-400 transform hover:scale-105"
+      onClick={onSelect}
+      className={`p-2 rounded-lg shadow-md transition-transform ${
+        isSelected ? "bg-blue-500 border-4 border-blue-500" : "bg-custom-gray"
+      } transform hover:scale-105 cursor-pointer`}
     >
       <h4 className="text-lg text-slate-50 font-semibold text-center mb-1">
         {locality.name}
@@ -24,18 +36,24 @@ export default function UserEventLocality({
       </p>
 
       {/* Quantity Selection */}
-      <div className="flex items-center justify-center space-x-2">
-        <label className="text-blue-400 font-medium" htmlFor="quantity">
-          <i className="fa-solid fa-ticket"></i> Quantity:
-        </label>
-        <input
-          type="number"
-          id="quantity"
-          min="1"
-          className="text-sm w-16 text-center p-1 bg-custom-dark border-2 border-blue-400 rounded-lg text-slate-50 focus:outline-none focus:ring-2 focus:ring-blue-400"
-          style={{ appearance: "textfield" }}
-        />
-      </div>
+      {isSelected && (
+        <div className="flex items-center justify-center space-x-2">
+          <label className="text-white font-medium" htmlFor="quantity">
+            <i className="fa-solid fa-ticket"></i> Quantity:
+          </label>
+          <input
+            type="number"
+            id="quantity"
+            min="1"
+            max={locality.maxCapacity - locality.currentOccupancy}
+            value={ticketsSelected}
+            onChange={(e) => setTicketsSelected(parseInt(e.target.value))}
+            className="text-sm w-16 text-center p-1 bg-custom-dark border-2 border-blue-400 rounded-lg text-slate-50 focus:outline-none focus:ring-2 focus:ring-blue-400"
+            disabled={isDisabled}
+            style={{ appearance: "textfield" }}
+          />
+        </div>
+      )}
     </div>
   );
 }
