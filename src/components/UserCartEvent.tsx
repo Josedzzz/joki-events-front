50aead2443c71117311e7547f5df1e3f2d7ac005
joki <josedavidamayar@gmail.com>
2024-10-10 22:37:47 -0500

    show more atributes about the locality on the client's cart

--- src/components/UserCartEvent.tsx
+++ src/components/UserCartEvent.tsx
@@ -1,28 +1,40 @@
-import { UserEvent } from "./UserEvents"
+import { LocalityOrder } from "../services/clientCartService";
 
 // Interface for the props of the component
 interface UserCartEventProps {
-  userEvent: UserEvent;
+  userEvent: LocalityOrder;
 }
 
 export default function UserCartEvent({ userEvent }: UserCartEventProps) {
   return (
-    <div className="bg-custom-dark rounded-2xl shadow-lg overflow-hidden max-w-xs border-4 border-transparent hover:border-blue-400 hover:scale-105 transition duration-300 ease-in-out"
-    >
+    <div className="bg-custom-dark rounded-2xl shadow-lg overflow-hidden max-w-xs border-4 border-transparent hover:border-blue-400 hover:scale-105 transition duration-300 ease-in-out">
+      {/* Event Image */}
       <img
         className="w-full h-32 sm:h-48 object-cover"
-        src={userEvent.imageUrl}
-        alt={userEvent.name}
+        src={userEvent.eventImageUrl}
+        alt={userEvent.eventName}
       />
+
+      {/* Event Information */}
       <div className="p-4">
         <h3 className="text-lg font-bold text-custom-white mb-2">
-          {userEvent.name}
+          {userEvent.eventName} - {userEvent.localityName}
         </h3>
         <p className="text-custom-white">
           {userEvent.address}, {userEvent.city}
         </p>
         <p className="text-gray-500 text-sm">
-          {new Date(userEvent.eventDate).toLocaleDateString()}
+          {new Date(userEvent.eventDate).toLocaleDateString()} -{" "}
+          {userEvent.eventType}
+        </p>
+
+        {/* Additional Information */}
+        <p className="text-custom-white mt-2">
+          <strong>Tickets Selected: </strong>
+          {userEvent.numTicketsSelected}
+        </p>
+        <p className="text-custom-white">
+          <strong>Total Payment: </strong>${userEvent.totalPaymentAmount}
         </p>
 
         {/* Remove Button */}
@@ -31,5 +43,5 @@ export default function UserCartEvent({ userEvent }: UserCartEventProps) {
         </button>
       </div>
     </div>
-  )
+  );
 }
