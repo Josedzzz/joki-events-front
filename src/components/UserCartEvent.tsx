abd60faba3c36cc1a6120a5411371962e6877542
joki <josedavidamayar@gmail.com>
2024-10-11 18:17:34 -0500

    call the service to delete the current order event

--- src/components/UserCartEvent.tsx
+++ src/components/UserCartEvent.tsx
@@ -1,4 +1,8 @@
-import { LocalityOrder } from "../services/clientCartService";
+import { useState } from "react";
+import {
+  deleteLocalityOrder,
+  LocalityOrder,
+} from "../services/clientCartService";
 
 // Interface for the props of the component
 interface UserCartEventProps {
@@ -6,6 +10,40 @@ interface UserCartEventProps {
 }
 
 export default function UserCartEvent({ userEvent }: UserCartEventProps) {
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  /**
+   * handles the deletion of an order on the clients cart
+   * @returns
+   */
+  const handleDeleteOrder = async () => {
+    setSuccess("");
+    setError("");
+
+    try {
+      if (!userEvent.eventId) {
+        setError("The order does not contain an Id");
+        return;
+      }
+      const response = await deleteLocalityOrder({
+        eventId: userEvent.eventId,
+        localityName: userEvent.localityName,
+        totalPaymentAmount: userEvent.totalPaymentAmount,
+        ticketsSelected: userEvent.numTicketsSelected,
+      });
+      setSuccess(response.message);
+      // reload the page after successfully deletion
+      window.location.reload();
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+      } else {
+        setError("An unexpected error occurred.");
+      }
+    }
+  };
+
   return (
     <div className="bg-custom-dark rounded-2xl shadow-lg overflow-hidden max-w-xs border-4 border-transparent hover:border-blue-400 hover:scale-105 transition duration-300 ease-in-out">
       {/* Event Image */}
@@ -38,9 +76,17 @@ export default function UserCartEvent({ userEvent }: UserCartEventProps) {
         </p>
 
         {/* Remove Button */}
-        <button className="mt-2 text-red-400 hover:text-red-600 transition-colors">
+        <button
+          className="mt-2 text-red-400 hover:text-red-600 transition-colors"
+          onClick={handleDeleteOrder}
+        >
           <i className="fa-solid fa-trash"></i> Remove
         </button>
+
+        {error && <p className="text-red-500 text-center mt-4">{error}</p>}
+        {success && (
+          <p className="text-green-500 text-center mt-4">{success}</p>
+        )}
       </div>
     </div>
   );
